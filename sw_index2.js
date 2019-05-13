0cd67a259b0530ccda2868581afde785532cbf67
James Renna <rennaj100@gmail.com>
2019-05-12 23:12:01 -0400

    final fixes

---
+++ sw_index2.js
@@ -0,0 +1,49 @@
+
+//cached area website enterily when offline to still display
+const cacheName = 'v1';
+
+//Call Install Event
+
+self.addEventListener('install', event => {
+    console.log('Service Worker: Installed');
+});
+
+//Call Activate Event
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker: Activated');
+    //Remove unwanted caches
+    event.waitUntil(
+        caches.keys()
+        .then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== cacheName) {
+                        console.log('Service Worker: Deleting Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
+//Call Fetch Event (Fetching the entire Site)
+
+self.addEventListener('fetch', event => {
+    console.log('Service Worker: Fetching from Cache');
+    event.respondWith(
+        fetch(event.request)
+        .then(response => {
+            //Clone the response
+            const responseReplica = response.clone();
+            //Open a Cache
+            caches.open(cacheName).then(cache => {
+                //Adding the response to cache
+                cache.put(event.request, responseReplica);
+            })
+            return response;
+        })
+        .catch((err) => caches.match(event.request).then(response=>response)));
+});
